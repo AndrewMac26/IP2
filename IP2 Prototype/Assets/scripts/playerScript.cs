573a0e7644d4dbc6f8e43c538587aa9869c27e72
AndrewMac26 <macfarlane.andrew98@gmail.com>
2017-02-15 21:41:34 +0000

    First Commit
    Week 4 Feb 15th

---
+++ IP2 Prototype/Assets/scripts/playerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D rigid;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		moveVelocity = moveInput * moveSpeed;
+
+		//rotate with controller
+		Vector3 playerDirection =  Vector3.forward * Input.GetAxisRaw("rHorizontal") * 5  + Vector3.forward * -Input.GetAxisRaw("rVertical") * 5;
+		if (playerDirection.sqrMagnitude > 0.0f) {
+			transform.Rotate(playerDirection);
+			//Debug.Log (playerDirection);
+		}
+	}
+	
+
+	void FixedUpdate()
+	{
+		rigid.velocity = moveVelocity; 
+	}
+}
